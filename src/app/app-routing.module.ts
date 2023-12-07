daad2549029815052bf31d5423c89f73e15461f4
mouhamadoufallbah <mouhamadoufall.bah@gmail.com>
2023-12-07 17:44:23 +0000

    Structure de base du projet

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './components/security/authentification/authentification.component';
+import { AcceuilComponent } from './components/mentores/acceuil/acceuil.component';
+import { AproposComponent } from './components/mentores/apropos/apropos.component';
+import { OrientationComponent } from './components/mentores/orientation/orientation.component';
+import { ContactComponent } from './components/mentores/contact/contact.component';
+import { MentorComponent } from './components/mentores/mentor/mentor.component';
+import { EvenementComponent } from './components/mentores/evenement/evenement.component';
+import { FaqComponent } from './components/mentores/faq/faq.component';
+import { MentionLegalComponent } from './components/mentores/mention-legal/mention-legal.component';
+import { ConfidentialiteComponent } from './components/mentores/confidentialite/confidentialite.component';
+import { ConditionUtilisationComponent } from './components/mentores/condition-utilisation/condition-utilisation.component';
+import { AcceuilMentorComponent } from './components/mentor/acceuil-mentor/acceuil-mentor.component';
+import { GestionDesMentorComponent } from './components/admin/gestion-des-mentor/gestion-des-mentor.component';
+import { GestionDesSessionsComponent } from './components/mentor/gestion-des-sessions/gestion-des-sessions.component';
+import { GestionDesMentoresComponent } from './components/mentor/gestion-des-mentores/gestion-des-mentores.component';
+import { AcceuilAdminComponent } from './components/admin/acceuil-admin/acceuil-admin.component';
+import { GestionDesEvenementsComponent } from './components/admin/gestion-des-evenements/gestion-des-evenements.component';
+import { GestionDesMetiersComponent } from './components/admin/gestion-des-metiers/gestion-des-metiers.component';
+
+const routes: Routes = [
+  //utilisateur simple
+  { path: 'login', component: AuthentificationComponent },
+  { path: 'acceuil', component: AcceuilComponent },
+  { path: 'apropos', component: AproposComponent },
+  { path: 'orientation', component: OrientationComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'mentor', component: MentorComponent },
+  { path: 'evenement', component: EvenementComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'mentioLegal', component: MentionLegalComponent },
+  { path: 'confidentialite', component: ConfidentialiteComponent },
+  { path: 'conditionUtilisation', component: ConditionUtilisationComponent },
+  { path: '', redirectTo: 'acceuil', pathMatch: 'full' },
+
+  //admin
+  { path: 'Dashboard-Admin', component: AcceuilAdminComponent },
+  { path: 'Dashboard-Admin-evenement', component: GestionDesEvenementsComponent },
+  { path: 'Dashboard-Admin-mentor', component: GestionDesMentorComponent },
+  { path: 'Dashboard-Admin-mentores', component: GestionDesMentoresComponent },
+  { path: 'Dashboard-Admin-metiers', component: GestionDesMetiersComponent },
+
+
+  //mentor
+  { path: 'Dashboard-Mentor-Accueil', component: AcceuilMentorComponent },
+  { path: 'Dashboard-Mentor-mentores', component: GestionDesMentoresComponent },
+  { path: 'Dashboard-Mentor-sessions', component: GestionDesSessionsComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
