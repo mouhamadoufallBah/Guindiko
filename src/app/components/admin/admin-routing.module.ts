1301ed32663e653ca9746d810cf47e160aebb065
mouhamadoufallbah <mouhamadoufall.bah@gmail.com>
2023-12-10 11:45:47 +0000

    Integration de toute les interface admin

--- src/app/components/admin/admin-routing.module.ts
+++ src/app/components/admin/admin-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GestionDesEvenementsComponent } from './gestion-des-evenements/gestion-des-evenements.component';
 import { GestionDesMentorComponent } from './gestion-des-mentor/gestion-des-mentor.component';
-import { GestionDesMentoresComponent } from '../mentor/gestion-des-mentores/gestion-des-mentores.component';
 import { GestionDesMetiersComponent } from './gestion-des-metiers/gestion-des-metiers.component';
 import { AcceuilAdminComponent } from './acceuil-admin/acceuil-admin.component';
 import { MainAdminComponent } from './main-admin/main-admin.component';
+import { GestionDesMentoresAdminComponent } from './gestion-des-mentores-admin/gestion-des-mentores-admin.component';
 
 const routes: Routes = [
   {
@@ -13,7 +13,7 @@ const routes: Routes = [
       { path: 'acceuil-admin', component: AcceuilAdminComponent },
       { path: 'dashboard-admin-mentor', component: GestionDesMentorComponent },
       { path: 'dashboard-admin-metiers', component: GestionDesMetiersComponent },
-      { path: 'dashboard-admin-mentores', component: GestionDesMentoresComponent },
+      { path: 'dashboard-admin-mentores', component: GestionDesMentoresAdminComponent },
       { path: 'dashboard-admin-evenement', component: GestionDesEvenementsComponent },
       { path: '', redirectTo: 'acceuil-admin', pathMatch: 'full' }
     ]
