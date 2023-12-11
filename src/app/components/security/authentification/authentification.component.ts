f212c93d20bdb05244bfc9a3a6c3ec9864b277ed
unknown <thiernohabibbah176@gmail.com>
2023-12-11 18:59:00 +0100

    pull authentification

--- src/app/components/security/authentification/authentification.component.ts
+++ src/app/components/security/authentification/authentification.component.ts
@@ -53,7 +53,7 @@ export class AuthentificationComponent implements OnInit {
     }else{
       this.authService.register(newMentor).subscribe(
         (data => {
-          
+
         })
       );
     }
